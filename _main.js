688a79ca5633eda1d7f243556959f8683497ea9e
Thomas Levine <occurrence@thomaslevine.com>
2013-10-27 17:59:50 +0000

    frequencies

--- _main.js
+++ _main.js
@@ -1,11 +1,11 @@
-window.loadSheet = function(data) {
+window.loadSheet = function(data, cb) {
   var girth = data.map(function(row) {return 1 * row.girth})
 
   var max = Math.max.apply(Math,girth)
   var min = Math.min.apply(Math,girth)
 
   girthScaled = girth.map(function(x){return (x - min)/(max - min) })
-  window.girthScaled = girthScaled
+  cb(girthScaled)
 }
 
 window.playSound = function(audio, time) {
--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 ;(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-window.main = function(data) {
+window.loadSheet = function(data, cb) {
   var girth = data.map(function(row) {return 1 * row.girth})
 
   var max = Math.max.apply(Math,girth)
   var min = Math.min.apply(Math,girth)
 
   girthScaled = girth.map(function(x){return (x - min)/(max - min) })
-  window.girthScaled = girthScaled
+  cb(girthScaled)
 }
 
 window.playSound = function(audio, time) {
