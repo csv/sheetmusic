87cdb15ee56ee6a1aa0337e0f6d7031cd74f1bb9
Thomas Levine <occurrence@thomaslevine.com>
2013-10-27 14:50:43 +0000

    oeu

--- _main.js
+++ _main.js
@@ -1,5 +1,12 @@
 var crossfilter = require('crossfilter');
 
 window.main = function(data) {
-  crossfilter(data)
+  var cf = crossfilter(data)
+  var girth = cf.dimension(function(row) {return row.girth})
+
+  var max = girth.top(1)[0].girth
+  var min = girth.bottom(1)[0].girth
+
+  g = girth.group(function(x){return (x - min)/(max - min) })
+  console.log(g.all())
 }
