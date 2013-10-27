b795ae0dd173c9997d10dacaf70286ba15f5854f
Thomas Levine <occurrence@thomaslevine.com>
2013-10-27 13:59:42 +0000

    keep tabletop in just the index.html

--- _main.js
+++ _main.js
@@ -1,9 +1,3 @@
-var tabletop = require('tabletop');
-
-(function(){
-  document.addEventListener('DOMContentLoaded', function() {
-    var gData
-    var KEY = "0Ao5u1U6KYND7dC1wekE1bThiaXF6dnBpMVBoV1VmR3c"
-    Tabletop.init( { key: KEY, callback: run, simpleSheet: true } ) 
-  }) 
-})()
+window.main = function(data) {
+  console.log(data)
+}
